171c06a944c5650486c72d61b718990f20b42ad8
Hazix <geoffrey.soulaques@epsi.fr>
2020-02-27 11:40:03 +0100

    test

--- UseCase/intervention_get_all.py
+++ UseCase/intervention_get_all.py
@@ -1,9 +1,26 @@
+from Domain.intervention import Intervention
 from Repository.intervention_repository import InterventionRepository
 
 
 class InterventionGetAllUseCase:
     def __init__(self, repo: InterventionRepository):
+        self.session = None
         self.repository = repo
 
     def execute(self):
         return self.repository.get_all()
+
+
+class ToDoSaveRequestObject:
+    def __init__(self, datatask):
+        if "description" not in datatask:
+            raise Exception("pas de description")
+        else:
+            self.__description = datatask["description"]
+
+    @property
+    def description(self):
+        return self.__description
+
+    def get_todo_task(self):
+        return Intervention(self.description)
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import pathlib
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
+from Domain.intervention import Intervention
 from Repository.intervention_db_repository import InterventionDbRepository
-from UseCase.intervention_get_all import InterventionGetAllUseCase
+from UseCase.intervention_get_all import InterventionGetAllUseCase, ToDoSaveRequestObject
 
 app = Flask(__name__)
 
@@ -17,12 +18,19 @@ def get_all_interventions():
     return jsonify(uc.execute())
 
 
-@app.route('/add', methods=['POST'])
-def add_intervention():
-    return ""
+@app.route("/add", methods=['POST'])
+def add():
+    try:
+        data_task = request.get_json(force=True)
+        task_request = ToDoSaveRequestObject(data_task)
+        # repo = TodoJsonRepository()
+        repo = InterventionDbRepository(CONNECTION_STRING)
+        uc = InterventionGetAllUseCase(repo)
+        response = uc.execute(task_request.get_todo_task())
+        return "{}".format(int(response.return_value)), {"Content-Type": "application/plaintext"}
+    except Exception as exc:
+        return str(exc), 400, {}
 
 
 if __name__ == '__main__':
     app.run()
-
-
